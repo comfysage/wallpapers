bac3dc8ff1a7928418cf7cb05079cef1c2fd8535
robin <comfysage@isabelroses.com>
2025-08-05 10:23:01 +0200

    fix(scripts): fix errors in simplify

--- .scripts/simplify.sh
+++ .scripts/simplify.sh
@@ -1,12 +1,62 @@
 #!/usr/bin/env bash
 
-[[ -z "$1" ]] && exit 1
+set -e
+
+# -- utils --
+
+msg() {
+  printf "\033[32;1m%s\033[m %s\n" "$PROMPT" "$*"
+}
+
+ok() {
+  >&2 printf "\033[34;1m%s \033[mok: %s\n" "$PROMPT" "$*"
+}
+
+warn() {
+  >&2 printf "\033[33;1m%s \033[mwarning: %s\n" "$PROMPT" "$*"
+}
+
+die() {
+  >&2 printf "\033[31;1m%s \033[merror: %s\n" "$PROMPT" "$*"
+  exit 1
+}
+
+confirm() {
+  >&2 printf "\033[33;1m%s \033[mconfirm? %s" "$PROMPT" "$CONFIRM_PROMPT"
+  read -n 1 -r ans
+  [[ ! "$ans" =~ ^[Yy]$ ]] && {
+    >&2 printf '%s\n' 'Exiting.'
+      exit 1
+    }
+  echo
+}
+
+ask() {
+  >&2 printf "\033[33;1m%s \033[(y/N) %s" "$PROMPT" "$CONFIRM_PROMPT"
+  read -n 1 -r ans
+  echo
+  [[ ! "$ans" =~ ^[Yy]$ ]] && {
+    return 1
+  }
+}
+
+has() {
+  if ! command -v "$1" &>/dev/null; then
+    return 1
+  fi
+}
+
+## -- main --
+
+[[ -z "$1" ]] && {
+  die "no path provided"
+}
 
 p="$( realpath "$1" )"
 
 _simplify_file() {
   item="$(realpath "$1")"
-  [[ -f "$item" ]] || exit 1
+  [[ -f "$item" ]] || return 1
   dir="$(dirname "$item")"
   basename=$(basename -- "$item")
   extension="${basename##*.}"
@@ -14,34 +64,34 @@ _simplify_file() {
 
   newname="$(rhash -H "$item" | cut -d ' ' -f1)"
 
-  [[ "$newname" == "$filename" ]] && exit 1
+  [[ "$newname" == "$filename" ]] && {
+    ok "skipping, name already set to hash ($filename)"
+    return 0
+  }
 
   newf="$dir/${newname}.${extension}"
-  # while [[ -f "$newf" ]]; do
-  #   newf="$dir/$(rhash).${extension}"
-  # done
+  [[ -f "$newf" ]] && {
+    warn "(conflict) file exists ($filename): $newname"
+    return 1
+  }
 
-  echo "$item -> $newf"
+  ok "$item -> $newf"
   mv "$item" "$newf"
 }
 
 _simplify_dir() {
   dir="$1"
   [[ -d "$dir" ]] || exit 1
-  echo "simplifying pictures in '$dir'"
+  msg "simplifying files in '$dir'"
 
   for item in "$dir"/*; do
     _simplify_file "$item"
   done
 }
 
-if ! command -v rhash 2>&1 >/dev/null
-then
-  echo "rhash could not be found"
-  exit 1
-fi
+has rhash || die "rhash could not be found"
 
-[[ -d "$p" ]] && { _simplify_dir "$p"; exit 0; }
-[[ -f "$p" ]] && { _simplify_file "$p"; exit 0; }
+[[ -d "$p" ]] && { _simplify_dir "$p" && exit 0 || exit 1; }
+[[ -f "$p" ]] && { _simplify_file "$p" && exit 0 || exit 1; }
 
-exit 1
+die "path not found"
