35b357e0ec68cef3c43353bc9c96b62049cf604d
robin <comfysagedev@gmail.com>
2026-04-11 21:12:15 +0200

    doc: update script

--- .scripts/generate_preview.sh
+++ .scripts/generate_preview.sh
@@ -1,36 +1,20 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # NOTE: this script should be run from the wallpapers root directory.
 
-nb_note="""
-## N.B.
-
-I did not create any of these wallpapers. If you are the original creator and would like for me
-to remove your work, please let me know."""
-
 generate_file() {
-  echo "# $dir"
-  for filename in $(fd '.*\.(jpg|jpeg|png)' -t f); do
-    printf "\n![%s](./%s)\n" "$filename" "$filename"
-  done
+	echo "# $1"
+	fd -I '.*\.(jpg|jpeg|png)' -t f | while read -r filename; do
+		printf '\n![%s](./%s)\n' "$filename" "$filename"
+	done
 }
 
-main() {
-  echo "# wallpapers" > README.md
-  echo "$nb_note" >> README.md
+printf "# wallpapers\n\n" >README.md
+cat desc.md >>README.md
 
-  for dir in $(ls -1d */); do
-    item="${dir%/}"
-    echo "\n[$item](./$item/README.md)\n" >> README.md
-    pushd $item
-    generate_file "$item" > README.md
-    popd
-  done
-}
-
-main $@
-
-# useful for inspecting readme after creation e.g. $ ./generate_preview.sh vim
-[ -n "$1" ] && $1 README.md
-
-exit 0
+find . -mindepth 1 -maxdepth 1 -type d -not -name '.*' | while read -r dir; do
+	item="${dir#./}"
+	item="${item%/}"
+	printf '\n[%s](./%s/README.md)\n' "$item" "$item" >>README.md
+	(cd "$item" && generate_file "$item" >README.md)
+done
---
+++ desc.md
@@ -0,0 +1,4 @@
+## N.B.
+
+I did not create any of these wallpapers. If you are the original creator and would like for me
+to remove your work, please let me know.
